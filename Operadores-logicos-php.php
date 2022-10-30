bf4dcdab1a8140f6f2d3a036c0e01eab5a00e9be
nuskacg <rodelugaliza@gmail.com>
2022-10-30 16:38:43 -0300

    OPERADORES LOGICOS nos permiten conectar dos o más valores para identificar si su valor

---
+++ Operadores-logicos-php.php
@@ -0,0 +1,65 @@
+<?php
+
+/* 3️⃣
+
+OPERADORES LOGICOS nos permiten conectar dos o más valores para identificar si su valor es positivo, negativo, true, false o nos ayuda a dar un sentido lógico a los enunciados que estemos desarrollando de acuerdo al caso.
+
+*Ejemplo
+    $valorA and $valorB 
+
+*Nombre
+    and
+
+*Resultado
+    true si valorA y valorB son true. 
+------------------------------------------------
+
+*Ejemplo
+    $valorA or $valorB
+
+*Nombre
+    or
+
+*Resultado
+    true si valorA o valorB son true.
+------------------------------------------------
+
+*Ejemplo
+    $valorA xor $valorB
+
+*Nombre
+    xor
+
+*Resultado
+    true si uno de los dos valores son true, pero no ambos.
+------------------------------------------------
+
+*Ejemplo
+    ! $valorA
+
+*Nombre
+    not
+
+*Resultado
+    true si valorA es false.
+------------------------------------------------
+*Ejemplo
+    $valorA && $valorB
+
+*Nombre
+    and
+
+*Resultado
+    true si valorA y $valorB son true.
+------------------------------------------------
+
+*Ejemplo
+    $valorA || $valorB      (  || alt+124)
+
+*Nombre
+    or
+
+*Resultado
+    true si valorA o $valorB son true.
+------------------------------------------------
+
