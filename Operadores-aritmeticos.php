e6567d48def741d16519f631f8e1b029719fa052
nuskacg <rodelugaliza@gmail.com>
2022-10-30 16:45:10 -0300

    Son aquellos que nos permiten trabajar con operaciones básicas

---
+++ Operadores-aritmeticos.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 5️⃣
+
+Operadores aritméticos con PHP
+Son aquellos que nos permiten trabajar con operaciones básicas.
+
+Suma, resta, multiplicación, división, módulo y potenciación.
+
+Suma: Para sumar dos valores signo +.
+Resta: Para restar dos valores signo -.
+Multiplicación: Para multiplicar dos valores signo *.
+División: Para dividir dos valores signo /.
+Incremento: Sirve para incrementar un valor numérico en 1, usamos el operador ++. Ejemplo: $value++, sumará 1 a $value.
+Decremento: Sirve para disminuir un valor numérico en 1, usamos el operador --. Ejemplo: $value--, restará 1 a $value.
+Módulo: Sirve para saber el resto de una división entera, usamos el operador %. Ejemplo: 10 % 3; da como resultado 1. Un uso muy polular de este operador es para saber si un número es par o impar:
+
+Documentación:
+
+https://www.php.net/manual/es/language.operators.arithmetic.php
+
+
+PHP: Operadores aritméticos - Manual
+
+https://www.php.net/manual/es/language.operators.arithmetic.php
+
