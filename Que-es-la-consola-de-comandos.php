02a75de47bb546e34ea889afb99c076fb5a6dced
nuskacg <rodelugaliza@gmail.com>
2022-10-30 16:14:17 -0300

    Introducción

---
+++ Que-es-la-consola-de-comandos.php
@@ -0,0 +1,11 @@
+<?php
+
+/* La consola de comando, es un programa que recibe ciertas palabras / códigos clave, y es interpretado para que suceda algo.
+
+En este caso vamos a trabajar con la terminal integrada en VScode.*/ 
+
+/* recomendados Cursos de PREWORK 
+https://platzi.com/cursos/prework-windows/
+
+*/
+
---
+++ entrenando-pensamiento-logico.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Cómo entrenar tu pensamiento lógico
+Formas de entrenar tu pensamiento lógico:
+
+Plataformas de retos online: Aquí algunos recursos (compartido por otro compañero).
+https://www.codewars.com/
+https://www.hackerrank.com/
+https://coderbyte.com
+https://www.codingame.com/
+https://codeo.app/ (código de registro quieroaprender)
+Diagramas de flujo
